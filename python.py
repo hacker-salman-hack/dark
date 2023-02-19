8d6f24c69fa407be3c13e0a9688b069bfab1c8c4
hacker-salman-hack <125825321+hacker-salman-hack@users.noreply.github.com>
2023-02-19 23:43:33 +0330

    Hacker
    hacker god

---
+++ python.py
@@ -0,0 +1,289 @@
+import colorama
+import os
+import pyfiglet
+os.system("pip install pyfiglet")
+os.system("pip install colorama")
+print('\033[94m')
+print(pyfiglet.figlet_format("PASORD "))
+mrhacker = input("\033[32mINTER PASORD_>>\033[31m")
+while mrhacker != 'dark' :
+	print("\033[31mPSORD ESHTBA\033[32m",mrhacker)
+	mrhacker = input("INTER PASORD_>>\033[31m")
+#Mr-dark
+#import
+import os
+import time
+import sys
+import datetime
+import requests
+
+
+# code colors
+red='\033[31m'  #قرمز
+green='\033[32m' #سبز
+blue='\033[36m' #ابی
+pink='\033[35m'  #بنفش
+rang='\033[34m' #ابی تیره
+yellow='/033[93m'  #زرد
+
+#dat
+dat = (datetime.datetime.today())
+yellow,green="\033[93m","\033[92m"
+
+
+#print_mohamad.herorist
+print(f"{green} ")
+banner = (f"""  salman hacker""")
+for bnr in banner:
+        sys.stdout.write(bnr)
+        sys.stdout.flush()
+        time.sleep(0.1)
+
+
+#clear
+os.system("clear")
+print()
+      
+            
+#baner
+print(f"{red} ")
+banner = (f"""      
+                        .xUHWH!! !!?MRDARK:.
+                        .X*#M@$!!  !X!M$$$$$$WWx:.
+                    :!!!!!!?H! :!$!$$$$$$$$$$8X:
+                    !!~  ~:~!! :~!$!#$$$$$$$$$$8X:
+                    :!~::!H!<   ~.U$X!?R$$$$$$$$MM!
+                    ~!~!!!!~~ .:XW$$$U!!?$$$$$$RMM!
+                    !:~~~ .:!M"T#$$$$WX??#MRSALMAN!
+                {blue}      ~?WuxiW*   "#$$$$8!!!!??!!!
+                    :X- M$$$$       "T#$T~!8$WUXU~
+                    :%  ~#$$$m:        ~!~ ?$$$$$$
+                :!.-   ~T$$$$8xx.  .xWW- ~""##*"
+        .....   -~~:< !    ~?T#$$@@W@*?$$      /
+        W$@@M!!! .!~~ !!     .:XUW$W!~ "~:    :
+        #"~~.:x%!!  !H:   !WM$$$$Ti.: .!WUn+!
+        :::~:!!:X~ .: ?H.!u "$$$B$$$!W:U!T$$M~
+        .~~   :X@!.-~   ?@WTWo("*$$$W$TH$!
+        Wi.~!X$?!-~    : ?$$$B$Wu("**$RM!
+        $R@i.~~ !     :   ~$$$$$B$$en:`
+        ?MXT@Wx.~    :     ~"##*$$$$M~
+
+	""")
+for bnr in banner:
+        sys.stdout.write(bnr)
+        sys.stdout.flush()
+        time.sleep(0.009)
+
+
+        
+print("")        
+print("")        
+print("")        
+print("")        
+        
+        
+######
+
+print(f"{rang} ")
+banner = (f"""\033[35m{dat}
+ \033[32m
+###############
+                      #################            #
+                   ######################         #
+                  #########################      #
+                ############################
+               ##############################
+               ###############################
+              ###############################
+              ##############################
+                              #    ########   #
+                 ##        ###        ####   ##
+                                      ###   ###
+                                    ####   ###
+               ####          ##########   ####
+               #######################   ####
+                 ####################   ####
+                  ##################  ####
+                    ############      ##
+                       ########        ###
+                      #########        #####
+                    ############      ######
+                   ########      #########
+                     #####       ########
+                       ###       #########
+                      ######    ############
+                     #######################
+                     #   #   ###  #   #   ##
+                     ########################
+                      ##     ##   ##     ##\033[31m""")
+
+
+input ("user rubika target:")
+print ("ok")
+
+for bnr in banner:
+        sys.stdout.write(bnr)
+        sys.stdout.flush()
+        time.sleep(0.009)
+
+
+print(" ")
+print(f"{rang} ")
+print(" code hasas ")
+time.sleep(0.1)
+print(" ")
+time.sleep(1.0)
+print("4.2.1.4") 
+time.sleep(0.1)
+print("1.3.1.5")
+time.sleep(1.0)
+print("7.5.8.3")
+time.sleep(0.1)
+print("5.4.6.4")
+time.sleep(1.0)
+print (" ")
+print("code filtering")
+time.sleep(0.1)
+print(" ")
+print("sazandeh = salman hacker , MRDARK")
+print(" ")
+print("id rubika = @Khasteh_to")
+print(" ")
+print("Dxprit = http://dscript-sxs22-87-99-typer-filteri.gigfa.com/dxprit.html")
+print(f"{red} ")      
+        
+            
+#start
+print("")
+print("")
+time.sleep(1.7)
+print ("$")
+time.sleep(0.5)
+print ("\033[32m$$")
+time.sleep(0.4)
+print ("\033[31m$$$")
+time.sleep(0.3)
+print ("\033[32m$$$$")
+time.sleep(0.2)
+print ("\033[31m$$$$$")
+time.sleep(0.1)
+print ("\033[32m$$$$$$")
+time.sleep(0.5)
+print ("\033[31m$$$$$$$")
+time.sleep(0.1)
+print ("\033[32m$$$$$$$$")
+time.sleep(0.5)
+print("\033[31m$$$$$$$$$")
+time.sleep(0.1)
+
+
+print("")
+print("")
+
+
+#input
+up = input("""\033[31m
+>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+\033[32m{cood filter account (1) }
+{cood filter channel (2) }
+{cood filtr group     (3) } 
+{Super code rubika  (4) } 
+{link sexi (5) }
+{code mokharab pythoni (6) }
+{code hangi rubika (7) }
+{tarjomh hasas filtering (8) }
+{bog god filtering (9) }
+{link mokharab filteri (10) }
+\033[31m>>>>>>>>>>>>>>>>>>>>>>>>>>
+coode ro entkhab kon \033[32m~~~ \033[31m>>>\033[34m""")
+
+print("https://rubika.ir/Khasteh_to")
+#!
+print(" ")
+print("Login......")
+time.sleep(1.5)
+
+print(" ")
+
+
+
+print(" ")
+print(" ")
+#cod filteri acoonut
+if up == "1":
+         print ("""\033[34mbog = (1.3.5.8///f///y//d/1.4.5.7.9.0.8.7.6.4.2.4.6.8.9.0.9.7.5.4.3.4.6.9.0.1)\033[34m""")
+   
+         
+                     
+#cod filteri channel     
+if up == "2":
+         print ("""\033[34mbog = (1.3.6.8//y///f///g/1.3.6.7.9.0.8.6.5.3.1.1.3.6.7.0.0.8.5.3.3.6.8.9.7)\033[34m""")
+         
+ 
+           
+#cod filteri group                 
+if up == "3":
+	
+        print("""\033[34mbog = (1.4.5.7///b///y//1.4.6.8.9.0.8.6.3.6.8.8.0.5.4.2.1.1.3.5.6.8.0.6.8.3)\033[34m""")
+
+
+
+#Super code rubika
+if up == "4":
+        print("""\033[34mSuper = (/5.6.7.filtering_rubika///6.5.3.8.h//310.561///5.1.1.6)\033[34m""")
+
+
+
+#link sexi
+if up == "5":
+        print("""\033[34msex = https://s4.uupload.ir/files/img_20210728_003953_172_jc2i.jpg\033[34m""")
+
+
+
+#code mokharab pythoni
+if up == "6":
+        print("""\033[34mmokharab = https://Filter.Filter.Filtering<akant.sex](http://filtering.rubika.python.py/)https://tex.sexi.000757.fil.akant.anlinXxx.porn.photo.tex.sexi.ey98.comXxx.fil.filter.akant.anlin.in.rubika.in.com(/yftt15k)=https://rubika/filly.ter/(10:19:21.621)abol.teror.Filtering.rubika.-yhttp://Dxprit.(27.0.0.1:9050.http://4.2.4.8/f////g.h/4.2.4.6.4.7.8.4.6.5.8.2.3.7.6.0.3.6.2)Filter.Filtering.com.by=gif.movi.sxsi\033[34m""")
+
+
+
+#code hangi rubika
+if up == "7":
+        print("""\033[34mhangi = 1.2.3.4.5.6.7.8.9.1.2.3.4.5.6.7.8.9.1.2.3.4.5.6.7.8.9.1.2.3.4.5.6.7.8.9.1.2.3.4.5.6.7.8.9.1.2.3.4.5.6.7.8.9.1.2.3.4.5.6.7.8.9.1.2.3.4.5.6.7.8.9.1.2.3.4.5.6.7.8.9.1.2.3.4.5.6.7.8.9.1.2.3.4.5.6.7.8.9.1.2.3.4.5.6.7.8.9.1.2.3.4.5.6.7.8.9.1.2.3.4.5.6.7.8.9.1.2.3.4.5.6.7.8.9.1.2.3.4.5.6.7.8.9.1.2.3.4.5.6.7.8.9.1.2.3.4.5.6.7.8.9.1.2.3.4.5.6.7.8.9.1.2.3.4.5.6.7.8.9.1.2.3.4.5.6.7.8.9.1.2.3.4.5.6.7.8.9.1.2.3.4.5.6.\033[34m""")
+        
+        
+        
+#tarjomh hasas filtering
+if up == "8":
+        print("""\033[34mmatn hasas = Insulting the entire Islamic Revolution Guards Corps, the army, the leadership's house, and the families of the martyrs of the Islamic Revolutio\033[34m""")
+        
+        
+        
+#bog god filtering
+if up == "9":
+        print("""\033[34mbog = (1.2.4.7///f///i///1.3.5.6.8.0.8.6.4.2.1.1.2.4.5.7.8.9.0.0.8.6.3.2.1)(1.4.6.9//f///d//1.3.5.7.8.7.0.9.7.5.4.3.2.2.4.6.7.8.3.5.7.9.5.7.9)(1.4.6.7//d//g//1.4.6.7.9.4.6.8.0.8.5.3.2.1.2.4.6.7.9.0.7.5.3.2.1)filtering.rubika\033[34m""")
+       
+       
+       
+#link mokharab filteri
+if up == "10":
+         print("""\033[34mcode filter = https://ter.af-ter.af-ter.(03.14:14.03.000).kode.code(https://bit.il/03ild93LYtTk_018. )(3.1.6.8/)rubika.fill.robika.fyll.*((7.8.9.2.d/f///y//6065///g.k.4.6.7.8.2.3.5.6.7.3.2.9.3.:bit.ly/3ild93L.9.1.5.3.6.2.9.6/.7.8.9.2.4.6.7.8.2.3.5.6.7.3.2.9.3.9.1.5.3.6.2.9.6/))(Af-aF-Af/af+fill+by+rubika+*+*+\033[34m""")
+print(" ")
+print(" ")
+print("salman hacker god")
+time.sleep(0.5)
+print(" ")
+print("eskript salman hacker")
+time.sleep(0.5)
+print(" ")                                  
+print("im god majazi")
+time.sleep(0.5)
+print(" ")
+print("id rubika = @Khasteh_to")
+time.sleep(0.5)
+print(" ")
+print("id shad = @Login111")
+time.sleep(0.5)
+print(" ")
+print("my'love , zahra🙂♥️")
+#my'love , zahra🙂♥️
+#mr dark
